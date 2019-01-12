3e33c9c8ab6523ab1dcf4aa786dd0d26660feef1
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-12 17:19:48 -0500

    Custom navigation solution for routing with help of next-routes module

---
+++ routes.js
@@ -0,0 +1,5 @@
+const routes = require("next-routes")(); // we need to invoke the funcion returned by next-routes
+
+
+// this will help export helpers for components to navigate with the routing
+module.exports = routes; 
