025197f83fcc7c968665077f22380d0c0049a89e
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-12 16:28:23 -0500

    new campaign submission to ethereum blockchain

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,24 +1,51 @@
-import React, {Component} from 'react';
-import { Form, Button } from 'semantic-ui-react';
-import Layout from '../../components/Layout';
+import React, { Component } from "react";
+import { Form, Button, Input } from "semantic-ui-react";
+import Layout from "../../components/Layout";
+import factory from "../../ethereum/factory"; // we need factory to access the ABI to interact with our contract
+import web3 from "../../ethereum/web3";
 
 class CampaignNew extends Component {
-    render() {
-        return (
-            <Layout>
-                <h3>Create a Campaign</h3>
+  state = {
+    minimumContribution: ""
+  };
 
-                <Form>
-                    <Form.Field>
-                        <label>Minimum Contribution</label>
-                        <input />
-                    </Form.Field>
+  // Anonymous funciton is used here because we want to preserve 'this' variable 
+  onSubmit= async (event) => {
+    event.preventDefault();
 
-                    <Button primary>Create!</Button>
-                </Form>
-            </Layout>
-        );
-    }
+    const accounts = await web3.eth.getAccounts();
+    await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+            from: accounts[0]
+        });
+
+  };
+
+  render() {
+    return (
+      <Layout>
+        <h3>Create a Campaign</h3>
+
+        {/* no parentheses after onSubmit because we are passing it an anonymous function that will be executed later */}
+        <Form onSubmit={this.onSubmit}>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={event =>
+                this.setState({ minimumContribution: event.target.value })
+              }
+            />
+          </Form.Field>
+
+          <Button primary>Create!</Button>
+        </Form>
+      </Layout>
+    );
+  }
 }
 
 export default CampaignNew;
