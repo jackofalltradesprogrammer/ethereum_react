5d24f45f7b3a011b05406b148122874baf5a5dfc
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-12 13:05:29 -0500

    formatting the code and stylesheet for Semantic UI

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,42 @@
-import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
-import factory from '../ethereum/factory'; //gets the instance to interact with the contract - web3 takes care of authentication
-
+import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
+import factory from "../ethereum/factory"; //gets the instance to interact with the contract - web3 takes care of authentication
 
 // class based component
 class CampaignIndex extends Component {
-    static async getInitialProps(){  // static is a class function and not instance. THis function is exclusive to NEST and is run without rendering the component
-        const campaigns = await factory.methods.getDeployedCampaigns().call();
+  static async getInitialProps() {
+    // static is a class function and not instance. THis function is exclusive to NEST and is run without rendering the component
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-        return  {campaigns} ; // this is provided as props to our component and is read as {campaigns : campaigns}
-    }
-    
-    renderCampaigns() {
-        const items = this.props.campaigns.map(address => { // we pass a function to map, that function is called once for every elementin the array
-            return {
-                header: address, //first thing to display on the card for Semantic UI 
-                description: <a>View campaigns</a>,
-                fluid: true       // it is used to stretch the card to cover the window screen available
-            };
-        });
+    return { campaigns }; // this is provided as props to our component and is read as {campaigns : campaigns}
+  }
 
-        return <Card.Group items={items} />;
-    }
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      // we pass a function to map, that function is called once for every elementin the array
+      return {
+        header: address, //first thing to display on the card for Semantic UI
+        description: <a>View campaigns</a>,
+        fluid: true // it is used to stretch the card to cover the window screen available
+      };
+    });
 
-    render() {
-        return <div>{this.renderCampaigns()}</div>;
-    }
+    return <Card.Group items={items} />;
+  }
+
+  render() {
+    return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css"
+        />
+        {this.renderCampaigns()}
+      </div>
+    );
+  }
 }
 
 // NEXT expect an export of react component
 
 export default CampaignIndex;
-
-
-
