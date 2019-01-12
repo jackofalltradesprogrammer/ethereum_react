3f8b52450a87950780d44dc66985f38c31b10497
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-12 15:57:32 -0500

    add the CSS stylesheet link to Head component of NEXT

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,21 @@
 // This component will be the root component of all our components in the web applicaiton
 import React from 'react';
 import { Container } from 'semantic-ui-react'; // this is used to maximize the width of the page
+import Head from 'next/head'; // Head component to manipulate <head> of html page
 import Header from './Header';
 
 export default (props) => { // our functional components get called with props arguments
   return  (
     <Container>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css"
+          />
+        </Head>
         <Header/>
         {/* All the JSX passes between <Layout></Layout> will be passed as children  */}
         {props.children}    
-        <h1>Im a footer</h1>
     </Container>
   );
 };
--- pages/show.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default () => {
-    return (
-        <h1> This is the Campaign list page !!!!</h1>
-    );
-}
