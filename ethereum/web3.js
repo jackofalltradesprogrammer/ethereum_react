3a540253f6ad2980ea9788aefb8205ffacf7ccf7
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-11 20:04:46 -0500

    web3 instance when in "browser+metamask" or on the server

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,6 +1,18 @@
 import Web3 from 'web3';
 
 // people definitely need to have Metamask for it to work
-const web3 = new Web3(window.web3.currentProvider);
+// const web3 = new Web3(window.web3.currentProvider);
+
+let web3; // we can resign this variable
+
+// Check if we are in the browser and if metamask is present
+if (typeof window !== 'undefined' && window.web3 !== 'undefined') {
+    web3 = new Web3(window.web3.currentProvider);
+}else { // we are on the server or  the user is not running metamask
+    const provider = new Web3.provider.HttpProvider(
+        https://rinkeby.infura.io/v3/a9ee51eb0dd04456942f5bde2edb91b1
+    );
+    web3 = new Web3(provider);
+}
 
 export default web3;
