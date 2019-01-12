84ced02c1c1393400a3c05c67c04d59bd7031526
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-12 12:58:30 -0500

    use semantic CARD to display campagin addresses

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import factory from '../ethereum/factory'; //gets the instance to interact with the contract - web3 takes care of authentication
 
 
@@ -10,8 +11,20 @@ class CampaignIndex extends Component {
         return  {campaigns} ; // this is provided as props to our component and is read as {campaigns : campaigns}
     }
     
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => { // we pass a function to map, that function is called once for every elementin the array
+            return {
+                header: address, //first thing to display on the card for Semantic UI 
+                description: <a>View campaigns</a>,
+                fluid: true       // it is used to stretch the card to cover the window screen available
+            };
+        });
+
+        return <Card.Group items={items} />;
+    }
+
     render() {
-        return <div>{this.props.campaigns[0]}</div>;
+        return <div>{this.renderCampaigns()}</div>;
     }
 }
 
