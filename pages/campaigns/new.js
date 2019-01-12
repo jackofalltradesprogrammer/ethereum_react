dbc4155b0eeacc6d439f9b76c96324eda892bfe9
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-12 16:43:54 -0500

    form error handling with tryCatch and Semantic UI

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,25 +1,29 @@
 import React, { Component } from "react";
-import { Form, Button, Input } from "semantic-ui-react";
+import { Form, Button, Input, Message } from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import factory from "../../ethereum/factory"; // we need factory to access the ABI to interact with our contract
 import web3 from "../../ethereum/web3";
 
 class CampaignNew extends Component {
   state = {
-    minimumContribution: ""
+    minimumContribution: '',
+    errorMessage: ''
   };
 
-  // Anonymous funciton is used here because we want to preserve 'this' variable 
-  onSubmit= async (event) => {
+  // Anonymous funciton is used here because we want to preserve 'this' variable
+  onSubmit = async event => {
     event.preventDefault();
 
-    const accounts = await web3.eth.getAccounts();
-    await factory.methods
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
         .createCampaign(this.state.minimumContribution)
         .send({
-            from: accounts[0]
+          from: accounts[0]
         });
-
+    } catch (err) {
+        this.setState({errorMessage: err.message});
+    }
   };
 
   render() {
@@ -27,8 +31,10 @@ class CampaignNew extends Component {
       <Layout>
         <h3>Create a Campaign</h3>
 
-        {/* no parentheses after onSubmit because we are passing it an anonymous function that will be executed later */}
-        <Form onSubmit={this.onSubmit}>
+        {/* no parentheses after onSubmit because we are passing it an anonymous function that will be executed later 
+            error property is used her to toggle Message Component on/off
+            !! it converts opp boolean twice*/}
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
@@ -40,7 +46,9 @@ class CampaignNew extends Component {
               }
             />
           </Form.Field>
-
+          
+          {/* Message Component from Semantic UI to display errors. THIS WON'T WORK UNLESS YOU ADD ERROR PROPERTY TO FORM */}
+          <Message error header="Oops!" content={this.state.errorMessage} />    
           <Button primary>Create!</Button>
         </Form>
       </Layout>
