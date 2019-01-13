6c46094f65b4abd5164615e59c6484e24b488260
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-13 12:36:21 -0500

    Set the navigation on the hearer for user to navigate

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+import { Link } from '../routes'; // Link is used to create anchor tags for navigation
 
 export default () => { // we need to export this component to be used by Layout.js
     return (
         //  double curly braces are used to tell that its javascript code with object literal inside
         <Menu style={{ marginTop:  '10px'}}> 
-            <Menu.Item>
-                CrowdCoin
-            </Menu.Item>
+            {/* a generic component that doesn't add anything of its own. It wraps its children with clickEvent handler to navigate */}
+            <Link route="/">
+                <a className="item">CrowdCoin</a>
+            </Link>
             {/* // the right side of the header */}
             <Menu.Menu position="right">
-                <Menu.Item>Campaigns</Menu.Item>
-
-                <Menu.Item>+</Menu.Item>
+                <Link route="/">
+                    <a className="item">Campaigns</a>
+                </Link>
+                <Link route="/campaigns/new">
+                    <a className="item">+</a>
+                </Link>
             </Menu.Menu>
         </Menu>
     );
