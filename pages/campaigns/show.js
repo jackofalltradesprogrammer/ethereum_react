95ac6263873d738ea0d5ccc3131a0c5eb7150a6e
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-13 13:36:51 -0500

    adding show to the Layout Root component

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,14 @@
 // This components shows the details of deployed contract
 import React, { Component } from 'react';
+import Layout from '../../components/Layout';
 
 class CampaignShow extends Component {
     render() {
-        return <h3>Campaign Show</h3>;
+        return (
+            <Layout>
+                <h3>Campaign Show</h3>
+            </Layout>
+        );
     }
 }
 
