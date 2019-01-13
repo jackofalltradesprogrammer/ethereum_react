f174999b84d90185223d8cee069f1f5c33abd7d0
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-13 14:13:51 -0500

    make a call to fetch some data from the contract

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,11 +1,16 @@
 // This components shows the details of deployed contract
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign'; // gets the arrow function defined in the campaign.js
 
 class CampaignShow extends Component {
     //props will have the access to url being passed 
     static async getInitialProps(props) {
-        console.log(props.query.address);
+        const campaign = Campaign(props.query.address);
+
+        const summary = await campaign.methods.getSummary().call();
+
+        console.log(summary);
 
         return {};
     }
