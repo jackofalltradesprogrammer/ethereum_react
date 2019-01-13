f10f4ecd79845540f0f931520776855b51143fa8
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-13 17:30:50 -0500

    Component to display the requests for a particular contract

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,10 +1,27 @@
 //to show List of Requests on this component
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import { Link } from '../../../routes';
+import Layout from '../../../components/Layout';
 
 class RequestIndex extends Component {
+    // this function provided by NEXT can be used to pull info from the address bar
+    static async getInitialProps(props) {
+        const { address} = props.query; // address variable is like a wildcard defined in the route.js
+
+        return { address }; // its read as address: address
+
+    }
     render() {
         return (
-            <h3>Request List</h3>
+            <Layout>
+                <h3>Requests</h3>
+                <Link route={`/campaigns/${this.props.address}/requests/new`}>
+                    <a>
+                        <Button primary>Add Request</Button>
+                    </a>
+                </Link>
+            </Layout>
         );
     }
 }
