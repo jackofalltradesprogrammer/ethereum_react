bd081164b36dfed9a6e10484e2ed86ff879adbca
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-13 13:10:01 -0500

    a component to show details of a campaign

---
+++ pages/campaigns/show.js
@@ -0,0 +1,10 @@
+// This components shows the details of deployed contract
+import React, { component } from 'react';
+
+class CampaignShow extends Component {
+    render() {
+        return <h3>Campaign Show</h3>;
+    }
+}
+
+export default CampaignShow;
