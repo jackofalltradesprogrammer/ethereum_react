290c2ea3b9d3748d5b6832756f019107193edd81
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-13 12:27:54 -0500

    adding some comment for more info

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 const routes = require("next-routes")(); // we need to invoke the funcion returned by next-routes
 
 
-// this will help export helpers for components to navigate with the routing
+// this will help export helpers for users to navigate and helps generate link tags in the components
 module.exports = routes; 
--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const { createServer } = require('http');
 const next = require('next');
 
 const app = next ({
-   dev: process.env.NODE_ENV !== 'production' 
+   dev: process.env.NODE_ENV !== 'production'  // production or development mode
 });
 
 const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
+//make the app listen to a port, a callback function for the error
 app.prepare().then(() => {
     createServer(handler).listen(3000, (err) => {
         if (err) throw err;
