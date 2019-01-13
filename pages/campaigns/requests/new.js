750adeb9ae246c7ab8f8ccad515610fc6e02f53c
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-13 18:28:28 -0500

    Polish the form with error handling and loading spinner

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -11,7 +11,9 @@ class RequestNew extends Component {
     state = {
         value: '',
         description: '',
-        recipient: ''
+        recipient: '',
+        loading: false,
+        errorMessage: ''
     };
 
     //we need the address of the Contract
@@ -28,24 +30,35 @@ class RequestNew extends Component {
         const campaign = Campaign(this.props.address);
         const { description, value, recipient } = this.state;
 
+        // toggle on the loading and clears if there is any error message
+        this.setState({ loading: true, errorMessage: ''})
+
         try {
             const accounts = await web3.eth.getAccounts();
             await campaign.methods
                 .createRequest( description, web3.utils.toWei(value, 'ether'), recipient)
                 .send({ from: accounts[0] });
-        } catch (err) {
 
+            Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
         }
+
+        this.setState ({ loading: false})
+
     };
 
     render() {
         return (
             <Layout>
+                <Link route={`/campaigns/${this.props.address}/requests`}>
+                    <a>Back</a>
+                </Link>
                 <h3>Create a Request </h3>
                 {/* no parentheses after onSubmit because we are passing it an anonymous function that will be executed later 
                  error property is used her to toggle Message Component on/off
                  !! it converts opp boolean twice*/}
-                <Form onSubmit={this.onSubmit}>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                     <Form.Field>
                         <label>Description</label>
                         <Input
@@ -70,7 +83,8 @@ class RequestNew extends Component {
                         />
                     </Form.Field>
 
-                    <Button primary>Create!</Button>
+                    <Message error header="Oops!" content={this.state.errorMessage} />
+                    <Button primary loading={this.state.loading }>Create!</Button>
                 </Form>
             </Layout>
         );
