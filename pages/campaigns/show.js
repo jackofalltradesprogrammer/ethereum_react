a47975a8e2f3e0676f131da10f09880658148cd4
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-13 14:27:51 -0500

    Add the Semantic UI Card style  to the details of a Campaign

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,5 +1,6 @@
 // This components shows the details of deployed contract
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign'; // gets the arrow function defined in the campaign.js
 
@@ -19,12 +20,33 @@ class CampaignShow extends Component {
         };
     }
 
-    
+    renderCards() {
+        // Assign the values from this.props to the variables
+        const {
+            balance,
+            manager,
+            minimumContribution,
+            requestsCount,
+            approversCount
+        } = this.props; 
+
+        const items = [
+            {
+                header: manager,
+                meta: 'Address of Manager',
+                description: 'The manager created this campaign and can create requests to withdraw money',
+                style: { overflowWrap: 'break-word' }
+            }
+        ];
+        // Card Group from semantic UI REACT
+        return <Card.Group items={items} />;
+    }
 
     render() {
         return (
             <Layout>
                 <h3>Campaign Show</h3>
+                {this.renderCards()}
             </Layout>
         );
     }
