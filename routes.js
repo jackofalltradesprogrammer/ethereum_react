9ec90334fdff2c73760248c7d03a1f8ed4a208d5
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-13 17:31:38 -0500

    Update the routes for list of requests and new request for Contracts

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const routes = require("next-routes")(); // we need to invoke the funcion return
 routes
     .add('/campaigns/new', '/campaigns/new')    // overrides the next route
     .add('/campaigns/:address', '/campaigns/show') // : defines its a wild card
-    .add('/campaigns/:address/requests', 'campaigns/requests/index');
+    .add('/campaigns/:address/requests', 'campaigns/requests/index') // Route to show the list of requests for that campaign
+    .add('/campaigns/:address/requests/new', 'campaigns/requests/new'); // the page to create a new request
 // this will help export helpers for users to navigate and helps generate link tags in the components
 module.exports = routes; 
