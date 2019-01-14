a538283d228b6361e28762af8031ce0897b2d0ca
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-13 20:37:07 -0500

    add more columns to the table and the approve request button

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
 import web3 from '../ethereum/web3';
 
 class RequestRow extends Component {
+    onApprove = async () => {
+
+    };
+
     render() {
         const { Row, Cell } = Table; //ES15 syntax to store Table.Row in Row etc
-        const { id, request } = this.props;
+        const { id, request, approversCount } = this.props;
         return (
             <Row>
                 <Cell>{id}</Cell>
                 <Cell>{request.description}</Cell>
                 <Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
                 <Cell>{request.recipient}</Cell>
+                <Cell>{request.approvalCount}/{approversCount}</Cell>
+                <Cell>
+                    {/* We don't use parentheses () after this.onApprove becuase we don't want to call it right away */}
+                    <Button color="green" basic onClick={this.onApprove}>
+                        Approve
+                    </Button>
+                </Cell>
             </Row>
         );
     }
