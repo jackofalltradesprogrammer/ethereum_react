f6d32e8cb72ad9cd80a2af9bdb714c8770d22c1f
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-01-13 19:57:18 -0500

    using fancy javascript get all the requests one by one from the contract

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -3,13 +3,24 @@ import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
+import Campaign from '../../../ethereum/campaign';
 
 class RequestIndex extends Component {
     // this function provided by NEXT can be used to pull info from the address bar
     static async getInitialProps(props) {
         const { address} = props.query; // address variable is like a wildcard defined in the route.js
+        const campaign = Campaign(address); // pass teh address to the arrow function to Campaign and get the instance
 
-        return { address }; // its read as address: address
+        const requestCount = await campaign.methods.getRequestsCount().call();
+        // Array(count).fill.map() runs number of times starting from 0 and more and gets the elements
+        const requests = await Promise.all(
+          Array(parseInt(requestCount)).fill().map((element, index) => {
+              return campaign.methods.requests(index).call()
+          })  
+        );
+
+
+        return { address, requests, requestCount }; // its read as address: address
 
     }
     render() {
